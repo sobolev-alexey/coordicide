d3fcb03b7b5dc7680e6f9467d0aca04417d13fea
Alexey Sobolev <sobolev@mail.de>
2019-05-16 15:25:18 +0200

    Added initial content holder component

---
+++ components/Content.js
@@ -0,0 +1,47 @@
+import markdown from 'markdown-in-js'
+
+export const Scalability = () => markdown`
+
+This is the Scalability
+
+`
+
+export const PostCoordinator = () => markdown`
+
+This is the PostCoordinator
+
+`
+
+export const Modularity = () => markdown`
+
+This is the Modularity
+
+`
+
+export const Modules = () => markdown`
+
+This is the Modules
+
+`
+
+export const Future = () => markdown`
+
+This is the Future
+
+`
+
+export const Conclusion = () => markdown`
+
+This is the Conclusion
+
+`
+
+export const Glossary = () => markdown`
+
+This is the Glossary
+
+`
+
+
+
+
