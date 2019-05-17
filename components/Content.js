2d3ddf25152d6cefc5b648a6912988db333079b5
Alexey Sobolev <sobolev@mail.de>
2019-05-17 13:58:53 +0200

    Updated content page

--- components/Content.js
+++ components/Content.js
@@ -2,37 +2,102 @@ import markdown from 'markdown-in-js'
 
 export const Scalability = () => markdown`
 
-This is the Scalability
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Imperdiet sed euismod nisi porta lorem mollis. Eu consequat ac felis donec et. Pellentesque id nibh tortor id aliquet lectus proin nibh. Non odio euismod lacinia at quis risus sed vulputate. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Consequat interdum varius sit amet. A iaculis at erat pellentesque adipiscing commodo elit. Urna et pharetra pharetra massa. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada. Senectus et netus et malesuada fames ac. Eget gravida cum sociis natoque.
+
+Ut eu sem integer vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Cum sociis natoque penatibus et magnis dis parturient montes. Posuere sollicitudin aliquam ultrices sagittis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Neque volutpat ac tincidunt vitae semper quis. Sapien pellentesque habitant morbi tristique senectus et.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Arcu ac tortor dignissim convallis. Pellentesque habitant morbi tristique senectus et netus. Lectus vestibulum mattis ullamcorper velit. Turpis egestas sed tempus urna et pharetra. Egestas integer eget aliquet nibh praesent tristique. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Amet purus gravida quis blandit turpis cursus. Posuere urna nec tincidunt praesent semper feugiat. Pellentesque dignissim enim sit amet. Consectetur libero id faucibus nisl tincidunt eget nullam non. Donec ultrices tincidunt arcu non sodales neque sodales ut. Scelerisque in dictum non consectetur. Vitae suscipit tellus mauris a diam. Euismod nisi porta lorem mollis. Quam nulla porttitor massa id. Vitae justo eget magna fermentum. Ut sem nulla pharetra diam sit. Tempor nec feugiat nisl pretium fusce id.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Vel turpis nunc eget lorem. In dictum non consectetur a. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Donec pretium vulputate sapien nec sagittis. Molestie at elementum eu facilisis sed odio morbi quis. Lorem mollis aliquam ut porttitor. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Consequat nisl vel pretium lectus quam id. Auctor eu augue ut lectus arcu bibendum at varius vel.
 
 `
 
 export const PostCoordinator = () => markdown`
 
-This is the PostCoordinator
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Imperdiet sed euismod nisi porta lorem mollis. Eu consequat ac felis donec et. Pellentesque id nibh tortor id aliquet lectus proin nibh. Non odio euismod lacinia at quis risus sed vulputate. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Consequat interdum varius sit amet. A iaculis at erat pellentesque adipiscing commodo elit. Urna et pharetra pharetra massa. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada. Senectus et netus et malesuada fames ac. Eget gravida cum sociis natoque.
+
+Ut eu sem integer vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Cum sociis natoque penatibus et magnis dis parturient montes. Posuere sollicitudin aliquam ultrices sagittis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Neque volutpat ac tincidunt vitae semper quis. Sapien pellentesque habitant morbi tristique senectus et.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Arcu ac tortor dignissim convallis. Pellentesque habitant morbi tristique senectus et netus. Lectus vestibulum mattis ullamcorper velit. Turpis egestas sed tempus urna et pharetra. Egestas integer eget aliquet nibh praesent tristique. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Amet purus gravida quis blandit turpis cursus. Posuere urna nec tincidunt praesent semper feugiat. Pellentesque dignissim enim sit amet. Consectetur libero id faucibus nisl tincidunt eget nullam non. Donec ultrices tincidunt arcu non sodales neque sodales ut. Scelerisque in dictum non consectetur. Vitae suscipit tellus mauris a diam. Euismod nisi porta lorem mollis. Quam nulla porttitor massa id. Vitae justo eget magna fermentum. Ut sem nulla pharetra diam sit. Tempor nec feugiat nisl pretium fusce id.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Vel turpis nunc eget lorem. In dictum non consectetur a. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Donec pretium vulputate sapien nec sagittis. Molestie at elementum eu facilisis sed odio morbi quis. Lorem mollis aliquam ut porttitor. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Consequat nisl vel pretium lectus quam id. Auctor eu augue ut lectus arcu bibendum at varius vel.
+
 
 `
 
 export const Modularity = () => markdown`
 
-This is the Modularity
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Imperdiet sed euismod nisi porta lorem mollis. Eu consequat ac felis donec et. Pellentesque id nibh tortor id aliquet lectus proin nibh. Non odio euismod lacinia at quis risus sed vulputate. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Consequat interdum varius sit amet. A iaculis at erat pellentesque adipiscing commodo elit. Urna et pharetra pharetra massa. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada. Senectus et netus et malesuada fames ac. Eget gravida cum sociis natoque.
+
+Ut eu sem integer vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Cum sociis natoque penatibus et magnis dis parturient montes. Posuere sollicitudin aliquam ultrices sagittis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Neque volutpat ac tincidunt vitae semper quis. Sapien pellentesque habitant morbi tristique senectus et.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Arcu ac tortor dignissim convallis. Pellentesque habitant morbi tristique senectus et netus. Lectus vestibulum mattis ullamcorper velit. Turpis egestas sed tempus urna et pharetra. Egestas integer eget aliquet nibh praesent tristique. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Amet purus gravida quis blandit turpis cursus. Posuere urna nec tincidunt praesent semper feugiat. Pellentesque dignissim enim sit amet. Consectetur libero id faucibus nisl tincidunt eget nullam non. Donec ultrices tincidunt arcu non sodales neque sodales ut. Scelerisque in dictum non consectetur. Vitae suscipit tellus mauris a diam. Euismod nisi porta lorem mollis. Quam nulla porttitor massa id. Vitae justo eget magna fermentum. Ut sem nulla pharetra diam sit. Tempor nec feugiat nisl pretium fusce id.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Vel turpis nunc eget lorem. In dictum non consectetur a. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Donec pretium vulputate sapien nec sagittis. Molestie at elementum eu facilisis sed odio morbi quis. Lorem mollis aliquam ut porttitor. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Consequat nisl vel pretium lectus quam id. Auctor eu augue ut lectus arcu bibendum at varius vel.
+
 
 `
 
 export const Modules = () => markdown`
 
-This is the Modules
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Imperdiet sed euismod nisi porta lorem mollis. Eu consequat ac felis donec et. Pellentesque id nibh tortor id aliquet lectus proin nibh. Non odio euismod lacinia at quis risus sed vulputate. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Consequat interdum varius sit amet. A iaculis at erat pellentesque adipiscing commodo elit. Urna et pharetra pharetra massa. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada. Senectus et netus et malesuada fames ac. Eget gravida cum sociis natoque.
+
+Ut eu sem integer vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Cum sociis natoque penatibus et magnis dis parturient montes. Posuere sollicitudin aliquam ultrices sagittis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Neque volutpat ac tincidunt vitae semper quis. Sapien pellentesque habitant morbi tristique senectus et.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Arcu ac tortor dignissim convallis. Pellentesque habitant morbi tristique senectus et netus. Lectus vestibulum mattis ullamcorper velit. Turpis egestas sed tempus urna et pharetra. Egestas integer eget aliquet nibh praesent tristique. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Amet purus gravida quis blandit turpis cursus. Posuere urna nec tincidunt praesent semper feugiat. Pellentesque dignissim enim sit amet. Consectetur libero id faucibus nisl tincidunt eget nullam non. Donec ultrices tincidunt arcu non sodales neque sodales ut. Scelerisque in dictum non consectetur. Vitae suscipit tellus mauris a diam. Euismod nisi porta lorem mollis. Quam nulla porttitor massa id. Vitae justo eget magna fermentum. Ut sem nulla pharetra diam sit. Tempor nec feugiat nisl pretium fusce id.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Vel turpis nunc eget lorem. In dictum non consectetur a. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Donec pretium vulputate sapien nec sagittis. Molestie at elementum eu facilisis sed odio morbi quis. Lorem mollis aliquam ut porttitor. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Consequat nisl vel pretium lectus quam id. Auctor eu augue ut lectus arcu bibendum at varius vel.
+
 
 `
 
 export const Future = () => markdown`
 
-This is the Future
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Imperdiet sed euismod nisi porta lorem mollis. Eu consequat ac felis donec et. Pellentesque id nibh tortor id aliquet lectus proin nibh. Non odio euismod lacinia at quis risus sed vulputate. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Consequat interdum varius sit amet. A iaculis at erat pellentesque adipiscing commodo elit. Urna et pharetra pharetra massa. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada. Senectus et netus et malesuada fames ac. Eget gravida cum sociis natoque.
+
+Ut eu sem integer vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Cum sociis natoque penatibus et magnis dis parturient montes. Posuere sollicitudin aliquam ultrices sagittis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Neque volutpat ac tincidunt vitae semper quis. Sapien pellentesque habitant morbi tristique senectus et.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Arcu ac tortor dignissim convallis. Pellentesque habitant morbi tristique senectus et netus. Lectus vestibulum mattis ullamcorper velit. Turpis egestas sed tempus urna et pharetra. Egestas integer eget aliquet nibh praesent tristique. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Amet purus gravida quis blandit turpis cursus. Posuere urna nec tincidunt praesent semper feugiat. Pellentesque dignissim enim sit amet. Consectetur libero id faucibus nisl tincidunt eget nullam non. Donec ultrices tincidunt arcu non sodales neque sodales ut. Scelerisque in dictum non consectetur. Vitae suscipit tellus mauris a diam. Euismod nisi porta lorem mollis. Quam nulla porttitor massa id. Vitae justo eget magna fermentum. Ut sem nulla pharetra diam sit. Tempor nec feugiat nisl pretium fusce id.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Vel turpis nunc eget lorem. In dictum non consectetur a. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Donec pretium vulputate sapien nec sagittis. Molestie at elementum eu facilisis sed odio morbi quis. Lorem mollis aliquam ut porttitor. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Consequat nisl vel pretium lectus quam id. Auctor eu augue ut lectus arcu bibendum at varius vel.
+
 
 `
 
 export const Conclusion = () => markdown`
 
-This is the Conclusion
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Imperdiet sed euismod nisi porta lorem mollis. Eu consequat ac felis donec et. Pellentesque id nibh tortor id aliquet lectus proin nibh. Non odio euismod lacinia at quis risus sed vulputate. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Consequat interdum varius sit amet. A iaculis at erat pellentesque adipiscing commodo elit. Urna et pharetra pharetra massa. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada. Senectus et netus et malesuada fames ac. Eget gravida cum sociis natoque.
+
+Ut eu sem integer vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Nam at lectus urna duis convallis convallis tellus id interdum. Cum sociis natoque penatibus et magnis dis parturient montes. Posuere sollicitudin aliquam ultrices sagittis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Neque volutpat ac tincidunt vitae semper quis. Sapien pellentesque habitant morbi tristique senectus et.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Arcu ac tortor dignissim convallis. Pellentesque habitant morbi tristique senectus et netus. Lectus vestibulum mattis ullamcorper velit. Turpis egestas sed tempus urna et pharetra. Egestas integer eget aliquet nibh praesent tristique. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Amet purus gravida quis blandit turpis cursus. Posuere urna nec tincidunt praesent semper feugiat. Pellentesque dignissim enim sit amet. Consectetur libero id faucibus nisl tincidunt eget nullam non. Donec ultrices tincidunt arcu non sodales neque sodales ut. Scelerisque in dictum non consectetur. Vitae suscipit tellus mauris a diam. Euismod nisi porta lorem mollis. Quam nulla porttitor massa id. Vitae justo eget magna fermentum. Ut sem nulla pharetra diam sit. Tempor nec feugiat nisl pretium fusce id.
+
+<img src="/static/q_oracle_assemblies.png"/>
+
+Vel turpis nunc eget lorem. In dictum non consectetur a. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Donec pretium vulputate sapien nec sagittis. Molestie at elementum eu facilisis sed odio morbi quis. Lorem mollis aliquam ut porttitor. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Consequat nisl vel pretium lectus quam id. Auctor eu augue ut lectus arcu bibendum at varius vel.
+
 
 `
 
--- pages/scalability.js
+++ pages/scalability.js
@@ -2,10 +2,15 @@ import Layout from '../components/Layout'
 import { Scalability } from '../components/Content'
 import PreviousChapter from '../components/PreviousChapter'
 import NextChapter from '../components/NextChapter'
+import Text from '../components/Text'
 import '../styles/content-page.scss'
 
 export default () => (
     <Layout>
+        <div className="content-header">
+            <Text className="content-title">Chapter 1</Text>
+            <Text className="content-subtitle">IOTA as<br />scalable DLT</Text>
+        </div>
         <div className="content">
             <Scalability />
         </div>
