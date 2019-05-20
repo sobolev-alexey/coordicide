0583b176dab1fde4d59fbaaf6f63857ea6c5e4b2
Alexey Sobolev <sobolev@mail.de>
2019-05-20 15:22:42 +0200

    Refactored chapters page

--- components/Chapters.js
+++ components/Chapters.js
@@ -4,49 +4,87 @@ import Text from './Text'
 
 import '../styles/chapters.scss'
 
-{/* <Link prefetch href="/">
-
-</Link> */}
-
-export default () => (
+export default ({ closeNav }) => (
     <div className="chapters-overlay">
-        <Text className="title extra-large">Chapters</Text>
+        <div className="close-nav" onClick={closeNav} />
+        <Text className="title extra-large">Chapter</Text>
         <div className="chapters-wrapper">
-            <div className="chapter">
-                <Text className="subtitle">Chapter 1</Text>
-                <Text className="subtitle light">IOTA as scalable DLT</Text>
-            </div>
-            <div className="chapter">
-                <Text className="subtitle">Chapter 2</Text>
-                <Text className="subtitle light">IOTA POST-COORDINATOR</Text>
-            </div>
-            <div className="chapter">
-                <Text className="subtitle">Chapter 3</Text>
-                <Text className="subtitle light">Modularity</Text>
-            </div>
-            <div className="chapter">
-                <Text className="subtitle">Chapter 4</Text>
-                <Text className="subtitle light">The Modules</Text>
-                <div className="subtopics">
+            <Link prefetch href="/scalability">
+                <div className="chapter">
+                    <Text className="subtitle">Chapter 1</Text>
+                    <Text className="subtitle light">IOTA as scalable DLT</Text>
+                </div>
+            </Link>
+            <Link prefetch href="/post-coordinator">
+                <div className="chapter">
+                    <Text className="subtitle">Chapter 2</Text>
+                    <Text className="subtitle light">IOTA POST-COORDINATOR</Text>
+                </div>
+            </Link>
+            <Link prefetch href="/modularity">
+                <div className="chapter">
+                    <Text className="subtitle">Chapter 3</Text>
+                    <Text className="subtitle light">Modularity</Text>
+                </div>
+            </Link>
+            <Link prefetch href="/modules">
+                <div className="chapter">
+                    <Text className="subtitle">Chapter 4</Text>
+                    <Text className="subtitle light">The Modules</Text>
+                </div>
+            </Link>
+
+            <div className="subtopics">
+                <Link prefetch href="/module1">
                     <div className="subtopic">
                         <Text className="subtitle">Module 1</Text>
                         <Text className="subtitle light">Node identities and mana</Text>
                     </div>
+                </Link>
+                <Link prefetch href="/module2">
                     <div className="subtopic">
                         <Text className="subtitle">Module 2</Text>
                         <Text className="subtitle light">Secure Auto-peering</Text>
                     </div>
+                </Link>
+                <Link prefetch href="/module3">
                     <div className="subtopic">
                         <Text className="subtitle">Module 3</Text>
                         <Text className="subtitle light">Span protection</Text>
                     </div>
+                </Link>
+                <Link prefetch href="/module4">
+                    <div className="subtopic">
+                        <Text className="subtitle">Module 4</Text>
+                        <Text className="subtitle light">Secure Auto-peering</Text>
+                    </div>
+                </Link>
+                <Link prefetch href="/module5">
+                    <div className="subtopic">
+                        <Text className="subtitle">Module 5</Text>
+                        <Text className="subtitle light">Span protection</Text>
+                    </div>
+                </Link>
+            </div>
+            
+            <Link prefetch href="/future">
+                <div className="chapter">
+                    <Text className="subtitle">Chapter 5</Text>
+                    <Text className="subtitle light">The Future</Text>
                 </div>
-            </div>
-            <div className="chapter">
-                <Text className="subtitle">Chapter 5</Text>
-                <Text className="subtitle light">The Future</Text>
-            </div>
+            </Link>
+            <Link prefetch href="/conclusion">
+                <div className="chapter">
+                    <Text className="subtitle">Chapter 6</Text>
+                    <Text className="subtitle light">Conclusion</Text>
+                </div>
+            </Link>
+            <Link prefetch href="/glossary">
+                <div className="chapter">
+                    <Text className="subtitle"></Text>
+                    <Text className="subtitle light">Glossary</Text>
+                </div>
+            </Link>
         </div>
     </div>
 )
-
--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,8 @@ import Menu from './Menu'
 import Chapters from './Chapters'
 import '../styles/layout.scss'
 
+export const MenuContext = React.createContext({});
+
 Router.onRouteChangeComplete = url => {
     gtag.pageview(url)
 }
@@ -27,7 +29,6 @@ export default ({ children, title = defaultTitle }) => {
     const [showOverlay, toggleShowOverlay] = useState(false);
 
     function showNav() {
-        console.log('showNav')
         toggleShowOverlay(true)
     }
     
@@ -36,49 +37,53 @@ export default ({ children, title = defaultTitle }) => {
     }
 
     return (
-        <div className="page">
-            <Head>
-                <title>{title}</title>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-            </Head>
-
-            <Link prefetch href="/">
-                <img className="mobile-logo" src="/static/iota_logo.svg" />
-            </Link>
-
-            <div className="show-nav" onClick={showNav} />
-            <div className="nav" id="nav">
-                <div className="close-nav" onClick={closeNav} />
-
-                <ExternalMenu
-                    className="nav-external"
-                    pages={externalPages}
-                    Link={Link}
-                />
-
-                <Menu onClick={showNav} />
-            </div>
-
+        <MenuContext.Provider value={{ showMenu: showNav }}>
             {
-                showOverlay ? <Chapters /> : children
+                showOverlay ? <Chapters closeNav={closeNav} /> : (
+                    <div className="page">
+                        <Head>
+                            <title>{title}</title>
+                            <meta charSet="utf-8" />
+                            <meta
+                                name="viewport"
+                                content="initial-scale=1.0, width=device-width"
+                            />
+                        </Head>
+
+                        <Link prefetch href="/">
+                            <img className="mobile-logo" src="/static/iota_logo.svg" />
+                        </Link>
+
+                        <div className="show-nav" onClick={showNav} />
+                        <div className="nav" id="nav">
+                            <div className="close-nav" onClick={closeNav} />
+
+                            <ExternalMenu
+                                className="nav-external"
+                                pages={externalPages}
+                                Link={Link}
+                            />
+
+                            <Menu onClick={showNav} />
+                        </div>
+
+                        { children }
+
+                        <footer>
+                            <img className="footer-iota-logo" src="/static/iota_logo.svg" />
+
+                            <p>Coordicide</p>
+                            <p>
+                                Powered by{' '}
+                                <a href="https://iota.org" target="_blank">
+                                    IOTA
+                                </a>
+                            </p>
+                            <Shape className="footer-shape" color="rgba(108, 228, 222, 0.2)" width="393px" height="416px" />
+                        </footer>
+                    </div>
+                )
             }
-
-            <footer>
-                <img className="footer-iota-logo" src="/static/iota_logo.svg" />
-
-                <p>Coordicide</p>
-                <p>
-                    Powered by{' '}
-                    <a href="https://iota.org" target="_blank">
-                        IOTA
-                    </a>
-                </p>
-                <Shape className="footer-shape" color="rgba(108, 228, 222, 0.2)" width="393px" height="416px" />
-            </footer>
-        </div>
+        </MenuContext.Provider>
     )
 }
