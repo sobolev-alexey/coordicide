6f635ea6643cabbfb68247a2f54210c43d50686c
Alexey Sobolev <sobolev@mail.de>
2019-05-21 18:10:14 +0200

    Applied new static assets to the components

--- components/Benefits.js
+++ components/Benefits.js
@@ -14,33 +14,32 @@ export default () => (
         </div>
         <div className="key-topic-wrapper">
             <div className="key-topic">
-                {/* <img src="../static/benefits/Feeless.svg" /> */}
-                <RoundIcon icon="key" />
+                <img src="../static/benefits/decentralized.png" />
                 <Text className="title">Decentralized & Permissionless</Text>
                 <Text>IOTA eliminates the need for miners and other centralized entities to reach consensus. It allows anybody to take part in the network in a completely trustless and permissionless way.</Text>
             </div>
             <div className="key-topic">
-                <RoundIcon icon="key" />
+                <img src="../static/benefits/finality.png" />
                 <Text className="title">Finality within Seconds</Text>
                 <Text>Transactions reach finality in seconds without having to wait for confirmations by external entities. A massively reduced need for reattachments increases the reliability of transactions.</Text>
             </div>
             <div className="key-topic">
-                <RoundIcon icon="key" />
+                <img src="../static/benefits/scalability.png" />
                 <Text className="title">Scalable & Lightweight</Text>
                 <Text>The absence of protocol related bottlenecks in combination with the ability of low-power devices to directly take part in the network enables the next industrial revolution - the Internet of Things.</Text>
             </div>
             <div className="key-topic">
-                <RoundIcon icon="key" />
+                <img src="../static/benefits/modular.png" />
                 <Text className="title">Modular & Future Proof</Text>
                 <Text>A completely modular design of the protocol enables layer-based extensions of the protocol and guarantees the ability to seamlessly adopt new findings from research in the field of DLT.</Text>
             </div>
             <div className="key-topic">
-                <RoundIcon icon="key" />
+                <img src="../static/benefits/feeless.png" />
                 <Text className="title">Feeless & Data Transaction</Text>
                 <Text>Feeless transactions enable the secure transfer of data and values between humans and machines, opening up new business models based on micro-payments.</Text>
             </div>
             <div className="key-topic">
-                <RoundIcon icon="key" />
+                <img src="../static/benefits/reliable.png" />
                 <Text className="title">Reliable Governance & Open Source</Text>
                 <Text>The nonprofit organization behind IOTA drives the adoption and evolution of its free and open source technology without any collusion of interests with 3rd parties like miners.</Text>
             </div>
--- components/Modules.js
+++ components/Modules.js
@@ -1,62 +1,96 @@
 import React from 'react'
 import Link from 'next/link'
+import Lottie from 'react-lottie'
 import Text from './Text'
-import Shape from './Shape';
+import Shape from './Shape'
+import * as module1 from '../static/modules/module_1.json'
+import * as module2 from '../static/modules/module_2.json'
+import * as module3 from '../static/modules/module_3.json'
+import * as module4 from '../static/modules/module_4.json'
+import * as module5 from '../static/modules/module_5.json'
 
 import '../styles/content.scss'
 
-export default () => (
-    <div className="modules-wrapper">
-        <div className="modules-heading-wrapper">
-            <Shape className="modules-shape" color="rgba(232, 242, 248, 0.4)" width="193px" height="226px" />
-            <Text className="title extra-large">BUILDING BLOCKS</Text>
-            <Text className="subtitle">of IOTA</Text>
+export default () => {
+
+    const options = {
+        loop: true,
+        autoplay: true,
+    };
+
+    return (
+        <div className="modules-wrapper">
+            <div className="modules-heading-wrapper">
+                <Shape className="modules-shape" color="rgba(232, 242, 248, 0.4)" width="193px" height="226px" />
+                <Text className="title extra-large">BUILDING BLOCKS</Text>
+                <Text className="subtitle">of IOTA</Text>
+            </div>
+            <div className="module">
+                <Lottie 
+                    options={{ ...options, animationData: module1.default }}
+                    height={170}
+                    width={300}
+                />
+                <Text className="subtitle">Module 1</Text>
+                <Text className="title">Node identities</Text>
+                <Text>Build a reputation and help secure the network by being a good actor.</Text>
+                <Link prefetch href="/node-identities">
+                    <a className="btn mini">Details</a>
+                </Link>
+            </div>
+            <div className="module">
+                <Lottie 
+                    options={{ ...options, animationData: module2.default }}
+                    height={160}
+                    width={250}
+                />
+                <Text className="subtitle">Module 2</Text>
+                <Text className="title">Auto-Peering</Text>
+                <Text>Connect to the network automatically, reducing the effort involved in running a node.</Text>
+                <Link prefetch href="/auto-peering">
+                    <a className="btn mini">Details</a>
+                </Link>
+            </div>
+            <div className="module">
+                <Lottie 
+                    options={{ ...options, animationData: module3.default }}
+                    height={160}
+                    width={278}
+                />
+                <Text className="subtitle">Module 3</Text>
+                <Text className="title">Spam Protection</Text>
+                <Text>Enable low-powered IoT devices to access the network through our adaptive rate control mechanism.</Text>
+                <Link prefetch href="/spam-protection">
+                    <a className="btn mini">Details</a>
+                </Link>
+            </div>
+            <div className="module">
+                <Lottie 
+                    options={{ ...options, animationData: module4.default }}
+                    height={160}
+                    width={278}
+                />
+                <Text className="subtitle">Module 4</Text>
+                <Text className="title">Tip Selection</Text>
+                <Text>Ensure your transaction gets picked up by the network and improve transaction times through an optimized tip selection algorithm.</Text>
+                <Link prefetch href="/tip-selection">
+                    <a className="btn mini">Details</a>
+                </Link>
+            </div>
+            <div className="module">
+                <Lottie 
+                    options={{ ...options, animationData: module5.default }}
+                    height={165}
+                    width={300}
+                />
+                <Text className="subtitle">Module 5</Text>
+                <Text className="title">Shimmer</Text>
+                <Text>Reach a consensus in seconds through a revolutionary voting module which leverages proactive communication in the network. This allows for irreversible transactions to be made within seconds.</Text>
+                <Link prefetch href="/shimmer">
+                    <a className="btn mini">Details</a>
+                </Link>
+            </div>
         </div>
-        <div className="module">
-            <img src="/static/modules/NodeIdentities.png" />
-            <Text className="subtitle">Module 1</Text>
-            <Text className="title">Node identities</Text>
-            <Text>Build a reputation and help secure the network by being a good actor.</Text>
-            <Link prefetch href="/node-identities">
-                <a className="btn mini">Details</a>
-            </Link>
-        </div>
-        <div className="module">
-            <img src="/static/modules/AutoPeering.png" />
-            <Text className="subtitle">Module 2</Text>
-            <Text className="title">Auto-Peering</Text>
-            <Text>Connect to the network automatically, reducing the effort involved in running a node.</Text>
-            <Link prefetch href="/auto-peering">
-                <a className="btn mini">Details</a>
-            </Link>
-        </div>
-        <div className="module">
-            <img src="/static/modules/SpamProtection.png" />
-            <Text className="subtitle">Module 3</Text>
-            <Text className="title">Spam Protection</Text>
-            <Text>Enable low-powered IoT devices to access the network through our adaptive rate control mechanism.</Text>
-            <Link prefetch href="/spam-protection">
-                <a className="btn mini">Details</a>
-            </Link>
-        </div>
-        <div className="module">
-            <img src="/static/modules/CompletelyDecentralized.png" />
-            <Text className="subtitle">Module 4</Text>
-            <Text className="title">Tip Selection</Text>
-            <Text>Ensure your transaction gets picked up by the network and improve transaction times through an optimized tip selection algorithm.</Text>
-            <Link prefetch href="/tip-selection">
-                <a className="btn mini">Details</a>
-            </Link>
-        </div>
-        <div className="module">
-            <img src="/static/modules/CompletelyDecentralized2.png" />
-            <Text className="subtitle">Module 5</Text>
-            <Text className="title">Shimmer</Text>
-            <Text>Reach a consensus in seconds through a revolutionary voting module which leverages proactive communication in the network. This allows for irreversible transactions to be made within seconds.</Text>
-            <Link prefetch href="/shimmer">
-                <a className="btn mini">Details</a>
-            </Link>
-        </div>
-    </div>
-)
+    )
+}
 
