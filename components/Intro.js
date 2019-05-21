a0bcf82d111e87c2859d47680c48e5486af8b957
Alexey Sobolev <sobolev@mail.de>
2019-05-21 18:12:24 +0200

    Updated intro page, added video and blurry background

--- components/Intro.js
+++ components/Intro.js
@@ -1,22 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import Link from 'next/link'
-import { MenuContext } from './Layout';
+import { MenuContext } from './Layout'
 import Text from './Text'
-import Shape from './Shape';
-import Button from './Button';
+import Shape from './Shape'
+import Button from './Button'
+import PlayButton from './PlayButton'
 
 import '../styles/content.scss'
 
 export default () => {
-    const { showMenu } = useContext(MenuContext);
+    const [showVideo, setShowVideo] = useState(false)
+    const { showMenu } = useContext(MenuContext)
+
     return (
         <div className="intro-wrapper">
-            <div className="intro">
-                <Shape className="intro-shape-outer" color="#ffffff" width="383px" height="434px" />
-                <Shape className="intro-shape-inner" color="rgba(232, 242, 248, 0.6)" width="193px" height="218px" />
-                <Text className="title">Coordicide</Text>
-                <Text className="subtitle">Removing the coordinator</Text>
-            </div>
+            {
+                showVideo ? (
+                    <div className="video-container aos-init aos-animate" data-aos="fade-up">
+                        <iframe
+                            className="video"
+                            src="https://www.youtube.com/embed/h09z2N0MtuQ"
+                            frameBorder="0"
+                            allow="autoplay; encrypted-media"
+                            allowFullScreen="">
+                        </iframe>
+                    </div> 
+                ) : (
+                    <div className="intro">
+                        <PlayButton onClick={() => setShowVideo(true)} />
+                        <div className="intro-blur-wrapper">
+                            <Shape className="intro-shape-outer" color="#ffffff" width="383px" height="434px" />
+                            <Shape className="intro-shape-inner" color="rgba(232, 242, 248, 0.6)" width="193px" height="218px" />
+                            <Text className="title">Coordicide</Text>
+                            <Text className="subtitle">Removing the coordinator</Text>
+                        </div>
+                    </div>
+                )
+            }
             <Text className="subtitle">Our goal</Text>
             <Text>At the IOTA Foundation, we are preparing to take the most significant step to date in the maturity of the protocol. This step in effect realizes the dream of a permissionless and scalable distributed ledger technology (DLT). We refer to this event as Coordicide -- <strong>the death of the Coordinator.</strong></Text>
             <div className="intro-buttons-wrapper">
--- pages/intro.js
+++ pages/intro.js
@@ -24,16 +24,6 @@ export default () => {
                         <Shape className="contact-form-right" color="rgba(108, 228, 222, 0.2)" width="393px" height="443px" />
                     </div>
                 </section>
-
-                {/* <div className="video-container aos-init aos-animate" data-aos="fade-up">
-                    <iframe
-                        className="video"
-                        src="https://www.youtube.com/embed/h09z2N0MtuQ"
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen="">
-                    </iframe>
-                </div> */}
             
                 <Shape className="right-shape" color="rgba(232, 242, 248, 0.3)" width="1000px" height="1020px" />
             </div>
